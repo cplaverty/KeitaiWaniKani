c649be6b69b6ad51ee5b8b9a8651f02e470bcdc2
cplaverty <cplaverty@users.noreply.github.com>
2015-10-08 23:19:51 +0100

    Add calculation of dates to Guru to SRSDataItem

--- WaniKaniKit/Model/SRSDataItem.swift
+++ WaniKaniKit/Model/SRSDataItem.swift
@@ -10,4 +10,95 @@ import Foundation
 public protocol SRSDataItem {
     var level: Int { get }
     var userSpecificSRSData: UserSpecificSRSData? { get }
+    
+    /// Returns the earliest date an item could be raised to Guru given the current state of the item
+    func guruDate(unlockDateForLockedItems: NSDate?) -> NSDate?
+    
+    /// Returns the earliest possible date an item could be raised to Guru, ignoring current progress and assuming all reviews correct
+    func earliestPossibleGuruDate(unlockDateForLockedItems: NSDate?) -> NSDate?
+}
+
+public extension SRSDataItem {
+    private static var isRadical: Bool { return self == Radical.self }
+    private var isAccelerated: Bool { return self.dynamicType.isRadical && level <= 2 }
+    
+    public func guruDate(unlockDateForLockedItems: NSDate?) -> NSDate? {
+        // Assume best case scenario: the next review is successful
+        guard let baseDate = userSpecificSRSData?.dateAvailable?.laterDate(NSDate()) ?? unlockDateForLockedItems else { return nil }
+
+        let initialLevel = (userSpecificSRSData?.srsLevelNumeric ?? 0) + 1
+        let guruNumericLevel = SRSLevel.Guru.numericLevelThreshold
+        
+        if initialLevel > guruNumericLevel { return nil }
+        else if initialLevel == guruNumericLevel { return baseDate }
+        
+        return minimumTimeFromLevel(initialLevel, toLevel: guruNumericLevel, fromDate: baseDate)
+    }
+    
+    public func earliestPossibleGuruDate(unlockDateForLockedItems: NSDate? = nil) -> NSDate? {
+        guard let baseDate = userSpecificSRSData?.dateUnlocked ?? unlockDateForLockedItems else { return nil }
+        
+        return minimumTimeFromLevel(1, toLevel: SRSLevel.Guru.numericLevelThreshold, fromDate: baseDate)
+    }
+    
+    private func minimumTimeFromLevel(initialLevel: Int, toLevel finalLevel: Int, fromDate baseDate: NSDate) -> NSDate? {
+        let isRadical = self.dynamicType.isRadical
+        let isAccelerated = self.isAccelerated
+        var guruDate = baseDate
+        let calendar = NSCalendar.autoupdatingCurrentCalendar()
+        for level in initialLevel..<finalLevel {
+            guard let timeForLevel = timeToNextReviewForLevel(level, isRadical: isRadical, isAccelerated: isAccelerated) else { return nil }
+            guruDate = calendar.dateByAddingComponents(timeForLevel, toDate: guruDate, options: .WrapComponents)!
+        }
+        
+        return guruDate
+    }
+    
+    private func timeToNextReviewForLevel(srsLevelNumeric: Int, isRadical: Bool, isAccelerated: Bool) -> NSDateComponents? {
+        switch srsLevelNumeric {
+        case 1 where isAccelerated:
+            let dc = NSDateComponents()
+            dc.hour = 2
+            return dc
+        case 1, 2 where isAccelerated:
+            let dc = NSDateComponents()
+            dc.hour = 4
+            return dc
+        case 2, 3 where isAccelerated:
+            let dc = NSDateComponents()
+            dc.hour = 8
+            return dc
+        case 3, 4 where isAccelerated:
+            let dc = NSDateComponents()
+            dc.day = 1
+            dc.hour = -1
+            return dc
+        case 4:
+            let dc = NSDateComponents()
+            dc.day = isRadical ? 2 : 3
+            dc.hour = -1
+            return dc
+        case 5: // -> 6
+            let dc = NSDateComponents()
+            dc.day = 7
+            dc.hour = -1
+            return dc
+        case 6: // -> 7
+            let dc = NSDateComponents()
+            dc.day = 14
+            dc.hour = -1
+            return dc
+        case 7: // -> 8
+            let dc = NSDateComponents()
+            dc.month = 1
+            dc.hour = -1
+            return dc
+        case 8: // -> 9
+            let dc = NSDateComponents()
+            dc.month = 4
+            dc.hour = -1
+            return dc
+        default: return nil
+        }
+    }
 }
---
+++ WaniKaniKitTests/SRSDataItemTests.swift
@@ -0,0 +1,219 @@
+//
+//  SRSDataItemTests.swift
+//  WaniKaniKit
+//
+//  Copyright © 2015 Chris Laverty. All rights reserved.
+//
+
+import XCTest
+@testable import WaniKaniKit
+
+class SRSDataItemTests: XCTestCase {
+    
+    func testGuruDateLockedKanji() {
+        let locked = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1)
+        
+        let lockedGuruDate = locked.guruDate(date(2016, 1, 1, 0, 0, 0))
+        let lockedExpectedGuruDate = date(2016, 1, 5, 10, 0, 0)
+        XCTAssertEqual(lockedGuruDate, lockedExpectedGuruDate)
+    }
+    
+    func testGuruDateApprenticeKanji() {
+        let newlyUnlocked = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let newlyUnlockedGuruDate = newlyUnlocked.guruDate(nil)
+        let newlyUnlockedExpectedGuruDate = date(2016, 2, 5, 6, 0, 0)
+        XCTAssertEqual(newlyUnlockedGuruDate, newlyUnlockedExpectedGuruDate)
+    }
+    
+    func testGuruDateApprentice3Kanji() {
+        let nearlyGuru = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: SRSLevel.Guru.numericLevelThreshold - 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let nearlyGuruDate = nearlyGuru.guruDate(nil)
+        let nearlyGuruExpectedDate = date(2016, 2, 1, 0, 0, 0)
+        XCTAssertEqual(nearlyGuruDate, nearlyGuruExpectedDate)
+    }
+    
+    func testGuruDateMasterKanji() {
+        let mastered = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Master,
+                srsLevelNumeric: SRSLevel.Master.numericLevelThreshold,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let masteredGuruDate = mastered.guruDate(nil)
+        let masteredExpectedGuruDate: NSDate? = nil
+        XCTAssertEqual(masteredGuruDate, masteredExpectedGuruDate)
+    }
+    
+    func testGuruDateLockedAcceleratedRadical() {
+        let locked = Radical(meaning: "", level: 1)
+        
+        let lockedGuruDate = locked.guruDate(date(2016, 1, 1, 0, 0, 0))
+        let lockedExpectedGuruDate = date(2016, 1, 2, 13, 0, 0)
+        XCTAssertEqual(lockedGuruDate, lockedExpectedGuruDate)
+    }
+    
+    func testGuruDateLockedNonAcceleratedRadical() {
+        let locked = Radical(meaning: "", level: 3)
+        
+        let lockedGuruDate = locked.guruDate(date(2016, 1, 1, 0, 0, 0))
+        let lockedExpectedGuruDate = date(2016, 1, 4, 10, 0, 0)
+        XCTAssertEqual(lockedGuruDate, lockedExpectedGuruDate)
+    }
+    
+    func testGuruDateApprenticeAcceleratedRadical() {
+        let newlyUnlocked = Radical(meaning: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let newlyUnlockedGuruDate = newlyUnlocked.guruDate(nil)
+        let newlyUnlockedExpectedGuruDate = date(2016, 2, 2, 11, 0, 0)
+        XCTAssertEqual(newlyUnlockedGuruDate, newlyUnlockedExpectedGuruDate)
+    }
+    
+    func testGuruDateApprenticeNonAcceleratedRadical() {
+        let newlyUnlocked = Radical(meaning: "",
+            level: 10,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let newlyUnlockedGuruDate = newlyUnlocked.guruDate(nil)
+        let newlyUnlockedExpectedGuruDate = date(2016, 2, 4, 6, 0, 0)
+        XCTAssertEqual(newlyUnlockedGuruDate, newlyUnlockedExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateLockedKanji() {
+        let locked = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1)
+        
+        let lockedGuruDate = locked.earliestPossibleGuruDate(date(2016, 1, 1, 0, 0, 0))
+        let lockedExpectedGuruDate = date(2016, 1, 5, 10, 0, 0)
+        XCTAssertEqual(lockedGuruDate, lockedExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateApprenticeKanji() {
+        let newlyUnlocked = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let newlyUnlockedGuruDate = newlyUnlocked.earliestPossibleGuruDate(nil)
+        let newlyUnlockedExpectedGuruDate = date(2016, 1, 5, 10, 0, 0)
+        XCTAssertEqual(newlyUnlockedGuruDate, newlyUnlockedExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateApprentice3Kanji() {
+        let nearlyGuru = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: SRSLevel.Guru.numericLevelThreshold - 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let nearlyGuruDate = nearlyGuru.earliestPossibleGuruDate(nil)
+        let nearlyGuruExpectedDate = date(2016, 1, 5, 10, 0, 0)
+        XCTAssertEqual(nearlyGuruDate, nearlyGuruExpectedDate)
+    }
+    
+    func testEarliestPossibleGuruDateMasterKanji() {
+        let mastered = Kanji(character: "",
+            meaning: "",
+            importantReading: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Master,
+                srsLevelNumeric: SRSLevel.Master.numericLevelThreshold,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let masteredGuruDate = mastered.earliestPossibleGuruDate(nil)
+        let masteredExpectedGuruDate = date(2016, 1, 5, 10, 0, 0)
+        XCTAssertEqual(masteredGuruDate, masteredExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateLockedAcceleratedRadical() {
+        let locked = Radical(meaning: "", level: 1)
+        
+        let lockedGuruDate = locked.earliestPossibleGuruDate(date(2016, 1, 1, 0, 0, 0))
+        let lockedExpectedGuruDate = date(2016, 1, 2, 13, 0, 0)
+        XCTAssertEqual(lockedGuruDate, lockedExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateLockedNonAcceleratedRadical() {
+        let locked = Radical(meaning: "", level: 3)
+        
+        let lockedGuruDate = locked.earliestPossibleGuruDate(date(2016, 1, 1, 0, 0, 0))
+        let lockedExpectedGuruDate = date(2016, 1, 4, 10, 0, 0)
+        XCTAssertEqual(lockedGuruDate, lockedExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateApprenticeAcceleratedRadical() {
+        let newlyUnlocked = Radical(meaning: "",
+            level: 1,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let newlyUnlockedGuruDate = newlyUnlocked.earliestPossibleGuruDate(nil)
+        let newlyUnlockedExpectedGuruDate = date(2016, 1, 2, 13, 0, 0)
+        XCTAssertEqual(newlyUnlockedGuruDate, newlyUnlockedExpectedGuruDate)
+    }
+    
+    func testEarliestPossibleGuruDateApprenticeNonAcceleratedRadical() {
+        let newlyUnlocked = Radical(meaning: "",
+            level: 10,
+            userSpecificSRSData: UserSpecificSRSData(srsLevel: SRSLevel.Apprentice,
+                srsLevelNumeric: 1,
+                dateUnlocked: date(2016, 1, 1, 0, 0, 0),
+                dateAvailable: date(2016, 2, 1, 0, 0, 0),
+                burned: false))
+        
+        let newlyUnlockedGuruDate = newlyUnlocked.earliestPossibleGuruDate(nil)
+        let newlyUnlockedExpectedGuruDate = date(2016, 1, 4, 10, 0, 0)
+        XCTAssertEqual(newlyUnlockedGuruDate, newlyUnlockedExpectedGuruDate)
+    }
+    
+}
