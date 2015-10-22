ad879dda3b3b8f2a085bc3d54f83ab2c930827b2
cplaverty <cplaverty@users.noreply.github.com>
2015-10-22 08:48:04 +0100

    Decrease size of character, increase size of home and stats divs

--- KeitaiWaniKani/User Scripts/resize.js
+++ KeitaiWaniKani/User Scripts/resize.js
@@ -1,7 +1,7 @@
 if (window.location.hostname.endsWith('wanikani.com') &&
     (window.location.pathname.startsWith('/review/session') || window.location.pathname.startsWith('/lesson/session'))) {
     addStyle(
-        '#reviews #question #character, #lessons header.quiz #main-info #character { font-size: 12vh !important; line-height: 20vh !important }' +
-        '@media (max-width: 767px) { #answer-form { font-size: 1.3em } }'
+        '#reviews #question #character, #lessons header.quiz #main-info #character { font-size: 10vh !important; line-height: 20vh !important }' +
+        '@media (max-width: 767px) { #answer-form { font-size: 1.3em } #summary-button, #stats { font-size: 16px } }'
     )
 }
