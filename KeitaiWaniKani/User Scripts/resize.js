bce21cc17a96bf0b4bfd7e9ac91b1e0f6db56355
cplaverty <cplaverty@users.noreply.github.com>
2015-10-23 00:12:22 +0100

    Increase line height and reduce padding

--- KeitaiWaniKani/User Scripts/resize.js
+++ KeitaiWaniKani/User Scripts/resize.js
@@ -1,7 +1,7 @@
 if (window.location.hostname.endsWith('wanikani.com') &&
     (window.location.pathname.startsWith('/review/session') || window.location.pathname.startsWith('/lesson/session'))) {
     addStyle(
-        '#reviews #question #character, #lessons header.quiz #main-info #character { font-size: 10vh !important; line-height: 15vh !important; padding: 20px 20px 0px; }' +
+        '#reviews #question #character, #lessons header.quiz #main-info #character { font-size: 10vh !important; line-height: 18vh !important; padding-top: 10px; }' +
         '@media (max-width: 767px) {' +
              '#answer-form { font-size: 1.25em; }' +
              '#summary-button, #header-buttons, #reviews #stats, #lessons #stats { font-size: 14px; }' +
