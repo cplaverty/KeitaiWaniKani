85a3137a00c3d42c2121ef23c141c84de84da725
cplaverty <cplaverty@users.noreply.github.com>
2015-10-24 00:50:46 +0100

    Add #answer-exception.answer-exception-form

--- KeitaiWaniKani/User Scripts/resize.js
+++ KeitaiWaniKani/User Scripts/resize.js
@@ -1,11 +1,12 @@
 if (window.location.hostname.endsWith('wanikani.com') &&
     (window.location.pathname.startsWith('/review/session') || window.location.pathname.startsWith('/lesson/session'))) {
     addStyle(
-        '#reviews #question #character, #lessons header.quiz #main-info #character { font-size: 10vh !important; line-height: 18vh !important; padding-top: 10px; }' +
+        '#reviews #question #character, #lessons header.quiz #main-info #character { font-size: 10vh; line-height: 18vh; padding-top: 10px; }' +
         '@media (max-width: 767px) {' +
              '#answer-form { font-size: 1.25em; }' +
              '#summary-button, #header-buttons, #reviews #stats, #lessons #stats { font-size: 14px; }' +
              '#answer-exception { font-size: 1em; }' +
+             '#answer-exception.answer-exception-form { top: 2.5em; }' +
         '}'
     )
 }
