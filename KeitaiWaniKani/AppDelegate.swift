5f9d1dbadec66693b78663ab998211b254b34d90
cplaverty <cplaverty@users.noreply.github.com>
2016-01-15 14:10:41 +0000

    App responds to kwk URL scheme

--- KeitaiWaniKani/AppDelegate.swift
+++ KeitaiWaniKani/AppDelegate.swift
@@ -46,20 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         if ApplicationSettings.purgeDatabase {
@@ -67,14 +53,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
 //    func applicationDidReceiveMemoryWarning(application: UIApplication) {
 //        DDLogDebug("Low memory warning: clearing statement cache")
 //        databaseQueue.inDatabase { $0.clearCachedStatements() }
 //    }
+    
+    func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+        DDLogInfo("Opening due to url \(url)")
+        return true
+    }
     
     // MARK: - Background fetch
     
