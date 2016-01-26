e15e7261332a4fc84f01b1aa18ede0a052d6f39a
cplaverty <cplaverty@users.noreply.github.com>
2016-01-26 21:34:24 +0000

    No need to dispatch to the main thread in Darwin notification callback since this is already called on the main thread

--- WaniKaniStudyQueueWidget/TodayViewController.swift
+++ WaniKaniStudyQueueWidget/TodayViewController.swift
@@ -46,9 +46,7 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
             { (_, observer, name, _, _) in
                 NSLog("Got notification for \(name)")
                 let mySelf = Unmanaged<TodayViewController>.fromOpaque(COpaquePointer(observer)).takeUnretainedValue()
-                dispatch_async(dispatch_get_main_queue()) { [weak mySelf] in
-                    mySelf?.updateStudyQueue()
-                }
+                mySelf.updateStudyQueue()
             },
             WaniKaniDarwinNotificationCenter.notificationNameForModelObjectType("\(StudyQueue.self)"),
             nil,
@@ -95,6 +93,7 @@ class TodayViewController: UITableViewController, NCWidgetProviding {
     // MARK: - Implementation
     
     func updateStudyQueue() {
+        assert(NSThread.isMainThread(), "Study queue update must be done on the main thread")
         if let studyQueue = try? self.fetchStudyQueueFromDatabase() {
             self.studyQueue = studyQueue
         }
