4ec42251996d5a50ea30be33c2bf1e8363530c34
cplaverty <cplaverty@users.noreply.github.com>
2016-02-24 21:55:10 +0000

    Add close button to hide timeout message when reviewing

--- KeitaiWaniKani/User Scripts/common.js
+++ KeitaiWaniKani/User Scripts/common.js
@@ -4,3 +4,12 @@ function GM_addStyle(cssContents) {
     style.appendChild(document.createTextNode(cssContents));
     document.head.appendChild(style);
 }
+
+// Add close button to timeout full-screen popup
+$('#timeout').prepend('<button id="timeout-close" type="button">&times;</button>');
+
+$(document).on('click', '#timeout-close', function() {
+    $('#timeout-idle').hide();
+    $('#timeout-session-end').hide();
+    $('#timeout').hide();
+});
