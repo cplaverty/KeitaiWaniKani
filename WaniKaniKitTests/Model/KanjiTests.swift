830ef33b61cbd7d33a4ac5b3dfa8de36ff93c94f
cplaverty <cplaverty@users.noreply.github.com>
2018-01-27 17:14:59 +0000

    Update test data

--- WaniKaniKitTests/Model/KanjiTests.swift
+++ WaniKaniKitTests/Model/KanjiTests.swift
@@ -12,7 +12,7 @@ class KanjiTests: XCTestCase {
     
     func testDecode() {
         let data = """
-            {"id":440,"object":"kanji","url":"https://www.wanikani.com/api/v2/subjects/440","data_updated_at":"2017-07-14T01:06:47.000000Z","data":{"level":1,"created_at":"2012-02-27T19:55:19.000000Z","slug":"一","document_url":"https://www.wanikani.com/kanji/%E4%B8%80","character":"一","meanings":[{"meaning":"One","primary":true}],"readings":[{"type":"Onyomi","primary":true,"reading":"いち"},{"type":"Kunyomi","primary":false,"reading":"ひと"},{"type":"Nanori","primary":false,"reading":"かず"}],"component_subject_ids":[1]}}
+            {"id":440,"object":"kanji","url":"https://www.wanikani.com/api/v2/subjects/440","data_updated_at":"2017-10-04T18:56:21.000000Z","data":{"level":1,"created_at":"2012-02-27T19:55:19.000000Z","slug":"一","document_url":"https://www.wanikani.com/kanji/%E4%B8%80","characters":"一","meanings":[{"meaning":"One","primary":true}],"readings":[{"type":"onyomi","primary":true,"reading":"いち"},{"type":"kunyomi","primary":false,"reading":"ひと"},{"type":"nanori","primary":false,"reading":"かず"}],"component_subject_ids":[1]}}
             """.data(using: .utf8)!
         
         let decoder = WaniKaniResourceDecoder()
@@ -23,15 +23,15 @@ class KanjiTests: XCTestCase {
             let expected = ResourceCollectionItem(id: 440,
                                                   type: .kanji,
                                                   url: URL(string: "https://www.wanikani.com/api/v2/subjects/440")!,
-                                                  dataUpdatedAt: makeUTCDate(year: 2017, month: 7, day: 14, hour: 1, minute: 6, second: 47),
+                                                  dataUpdatedAt: makeUTCDate(year: 2017, month: 10, day: 4, hour: 18, minute: 56, second: 21),
                                                   data: Kanji(level: 1,
                                                               createdAt: makeUTCDate(year: 2012, month: 2, day: 27, hour: 19, minute: 55, second: 19),
                                                               slug: "一",
                                                               character: "一",
                                                               meanings: [Meaning(meaning: "One", isPrimary: true)],
-                                                              readings: [Reading(type: "Onyomi", reading: "いち", isPrimary: true),
-                                                                         Reading(type: "Kunyomi", reading: "ひと", isPrimary: false),
-                                                                         Reading(type: "Nanori", reading: "かず", isPrimary: false)],
+                                                              readings: [Reading(type: "onyomi", reading: "いち", isPrimary: true),
+                                                                         Reading(type: "kunyomi", reading: "ひと", isPrimary: false),
+                                                                         Reading(type: "nanori", reading: "かず", isPrimary: false)],
                                                               componentSubjectIDs: [1],
                                                               documentURL: URL(string: "https://www.wanikani.com/kanji/%E4%B8%80")!))
             
--- WaniKaniKitTests/Model/RadicalTests.swift
+++ WaniKaniKitTests/Model/RadicalTests.swift
@@ -12,7 +12,7 @@ class RadicalTests: XCTestCase {
     
     func testDecode() {
         let data = """
-            {"id":1,"object":"radical","url":"https://www.wanikani.com/api/v2/subjects/1","data_updated_at":"2017-06-12T23:21:17.000000Z","data":{"level":1,"created_at":"2012-02-27T18:08:16.000000Z","slug":"ground","document_url":"https://www.wanikani.com/radicals/ground","character":"一","character_images":[],"meanings":[{"meaning":"Ground","primary":true}]}}
+            {"id":1,"object":"radical","url":"https://www.wanikani.com/api/v2/subjects/1","data_updated_at":"2018-01-24T23:08:17.000000Z","data":{"level":1,"created_at":"2012-02-27T18:08:16.000000Z","slug":"ground","document_url":"https://www.wanikani.com/radicals/ground","characters":"一","character_images":[],"meanings":[{"meaning":"Ground","primary":true}]}}
             """.data(using: .utf8)!
         
         let decoder = WaniKaniResourceDecoder()
@@ -23,7 +23,7 @@ class RadicalTests: XCTestCase {
             let expected = ResourceCollectionItem(id: 1,
                                                   type: .radical,
                                                   url: URL(string: "https://www.wanikani.com/api/v2/subjects/1")!,
-                                                  dataUpdatedAt: makeUTCDate(year: 2017, month: 6, day: 12, hour: 23, minute: 21, second: 17),
+                                                  dataUpdatedAt: makeUTCDate(year: 2018, month: 1, day: 24, hour: 23, minute: 8, second: 17),
                                                   data: Radical(level: 1,
                                                                 createdAt: makeUTCDate(year: 2012, month: 2, day: 27, hour: 18, minute: 8, second: 16),
                                                                 slug: "ground",
